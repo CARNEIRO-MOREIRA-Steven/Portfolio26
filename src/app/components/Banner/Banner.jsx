79a0add4cb2fb7cc09ec614d4cf4ae2ef63ec2a1
CARNEIRO-MOREIRA-Steven <carneiromoreirasteven22@gmail.com>
2025-12-30 13:28:13 +0100

    Modification Banner

--- src/app/components/Banner/Banner.jsx
+++ src/app/components/Banner/Banner.jsx
@@ -6,7 +6,7 @@ const Banner = () => {
     <section className='banner'>
         <p>Développeur web Fullstack</p>
         <h1>Développeur passionné <br></br>React.js / Next.js</h1>
-        <h2>Je suis CARNEIRO-MOREIRA Steven, basé à Chauny</h2>
+        <h2>Je suis Steven CARNEIRO-MOREIRA, basé à Chauny</h2>
         <a href="">Voir mes projets</a>
     </section>
   )
