817187d72726202e7be1a741b568a37ce986f29a
CARNEIRO-MOREIRA-Steven <carneiromoreirasteven22@gmail.com>
2025-12-31 16:06:07 +0100

    Ajustement design grid

--- src/app/components/Intro/Intro.tsx
+++ src/app/components/Intro/Intro.tsx
@@ -1,32 +1,56 @@
+"use client"
 import React from 'react'
 import './intro.css'
 
+import { useState } from "react";
+
 const Intro = () => {
   const myStack = [
-    "React.js",
-    "Next.js",
     "JavaScript",
     "TypeScript",
+    "React.js",
+    "Next.js",
     "Figma",
     "Redux"
   ];
 
+  const [copied, setCopied] = useState(false);
+  const email = "codemetasolutions@gmail.com";
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(email)
+      .then(() => {
+        setCopied(true);
+        setTimeout(() => setCopied(false), 2000); // remet à false après 2s
+      })
+      .catch(err => console.error("Erreur copie :", err));
+  };
+
   return (
-    <section className='container-grid '>
-        <div className='div1'></div>
-        <div className='div2'></div>
+    <section className='container-grid' id='about'>
+        <div className='div1'>
+          <img src="./Button.png" alt="" className='intro-button'/>
+          <h2>Passionné par la création d’interfaces modernes.
+          J’aime transformer des idées en expériences web.</h2>
+          <img src="./Button2.png" alt="" className='intro-button2'/>
+        </div>
+        <div className='div2'>
+          <h2>Disponible pour collaborer dans le monde entier.</h2>
+        </div>
         <div className='div3'>
             <div>
-            <h2>Ma stack technologique</h2>
-            <h3>Je m'efforce constamment de m'améliorer</h3>
-        <aside className='container-stack'>
-          {myStack.map((tech, index) => (
-            <span key={index} className="stack-item">
-              {tech}
-            </span>
-          ))}
-        </aside>
-        </div>
+            <h2>Ma stack tech</h2>
+            <h3>En constante amélioration</h3>
+            <aside className='container-stack'>
+              <div className='container-stack-grid'>
+              {myStack.map((tech, index) => (
+                <span key={index} className="stack-item">
+                  {tech}
+                </span>
+              ))}
+              </div>
+            </aside>
+            </div>
         </div>
 
         <div className='div4'>
@@ -34,12 +58,13 @@ const Intro = () => {
             <img src="./input.png" alt="" />
         </div>
         <div className='div5'>
-            Souhaitez-vous démarrer un projet ensemble ?
-            <button><img src="./Card.png" alt="" />Copier mon mail</button>
+            <h2>Souhaitez-vous démarrer un projet ensemble ?</h2>
+            <a className='button-mail' onClick={handleCopy}>{copied ? "Copié !" : "Copier mon email"}<img src="./Card.png" alt="" /></a>
         </div>
         <div className='div6'>
-            <h2>J'aime développer avec des <br></br>
-            bibliothèque d'animations JS</h2>
+            <h2>Création de sites web <br></br> animés et dynamiques</h2>
+            <h3>Ce qui se passe</h3>
+            <img src="./AnimeJS.png" alt="" className='image-code'/>
         </div>
     </section>
   )
